c8f30701064cfea63dad834132ffc977e3a96c2e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-06 15:41:02 -0700

    feat: added heroku image by default

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import * as NodeNotifier from 'node-notifier'
+import * as path from 'path'
 
 export interface Notification extends NodeNotifier.Notification {
   force?: boolean
 }
 
 export function notify(notification: Notification = {}, callback?: NodeNotifier.NotificationCallback) {
-  if (!notification.force || ['0', 'false'].includes(process.env.HEROKU_NOTIFICATIONS!)) return
-  return NodeNotifier.notify(notification, callback)
+  if (!notification.force && ['0', 'false'].includes(process.env.HEROKU_NOTIFICATIONS!)) return
+  return NodeNotifier.notify({
+    icon: path.join(__dirname, '../assets/heroku.png'),
+    ...notification,
+  } as Notification, callback)
 }
