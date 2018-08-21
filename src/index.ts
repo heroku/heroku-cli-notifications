cab659123ad8df4fdbfaff38bb6508360cd4c943
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-08-21 14:43:36 -0700

    fix: set app name

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ export interface Notification extends NodeNotifier.Notification {
 export function notify(notification: Notification = {}, callback?: NodeNotifier.NotificationCallback) {
   if (!notification.force && ['0', 'false'].includes(process.env.HEROKU_NOTIFICATIONS!)) return
   return NodeNotifier.notify({
+    title: 'Heroku CLI',
     icon: path.join(__dirname, '../assets/heroku.png'),
+    appName: 'Microsoft.Windows.ShellExperienceHost_cw5n1h2txyewy!App',
     ...notification,
   } as Notification, callback)
 }
