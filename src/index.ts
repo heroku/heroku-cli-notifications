40a77b0e2f903d90b20ac4ae2c94e89e13c68121
Eric Black <eblack@salesforce.com>
2025-02-21 11:40:08 -0800

    Update to node-notifier 10, update everything to node 20, update testing/linting things

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import * as NodeNotifier from 'node-notifier'
-import * as path from 'path'
-
-export interface Notification extends NodeNotifier.Notification {
+import * as path from 'node:path'
+import {Notification as NodeNotification, NotificationCallback, notify as nodeNotify} from 'node-notifier'
+export interface Notification extends NodeNotification {
   force?: boolean
 }
 
-export function notify(notification: Notification = {}, callback?: NodeNotifier.NotificationCallback) {
+export function notify(notification: Notification = {}, callback?: NotificationCallback) {
   if (!notification.force && ['0', 'false'].includes(process.env.HEROKU_NOTIFICATIONS!)) return
-  return NodeNotifier.notify({
-    title: 'Heroku CLI',
-    icon: path.join(__dirname, '../assets/heroku.png'),
+  return nodeNotify({
     appName: 'Microsoft.Windows.ShellExperienceHost_cw5n1h2txyewy!App',
+    icon: path.join(__dirname, '../assets/heroku.png'),
+    title: 'Heroku CLI',
     ...notification,
   } as Notification, callback)
 }
